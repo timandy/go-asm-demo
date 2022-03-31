7092a25d50cdb4a32a9a0569f1ed91ced552888c
TimAndy <xuchonglei@126.com>
2022-03-31 15:44:45 +0800

    dump arm64

--- dump.bat
+++ dump.bat
@@ -1,8 +1,8 @@
 
 SET CGO_ENABLED=0
 SET GOOS=linux
-SET GOARCH=amd64
+SET GOARCH=arm64
 del result.s /S /Q
 pause
-go build -gcflags="-N -l"
-go tool objdump -S  .\routine-demo > result.s
+go build -gcflags="-N -l" -o .\main.exe
+go tool objdump -S  .\main.exe > result.s
