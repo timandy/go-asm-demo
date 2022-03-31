051c62b0853d17e74b9110d798687ef7606700b0
Tim <xuchonglei@126.com>
2022-03-31 23:17:28 +0800

    Update getg0_arm.s

--- getg0_arm.s
+++ getg0_arm.s
@@ -3,19 +3,7 @@
 
 TEXT  ·getg0(SB), NOSPLIT, $8-8
     NO_LOCAL_POINTERS
-    MOVW $0, R8
-    MOVW $0, R9
-    MOVW R8, ret_type+0(FP)
-    MOVW R9, ret_data+8(FP)
-    GO_RESULTS_INITIALIZED
 
     // get runtime.g type
-    MOVW $type·runtime·g(SB), R8
-
-    // get runtime·g0 variable
-    MOVW runtime·g0(SB), R9
-
-    // return interface{}
-    MOVW R8, ret_type+0(FP)
-    MOVW R9, ret_data+8(FP)
+    MOVW $type·runtime·g(SB), R0
     RET
