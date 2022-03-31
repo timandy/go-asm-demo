a00769e1df04d92a2afda4e656696fc423e4b6d0
TimAndy <xuchonglei@126.com>
2022-03-31 15:44:56 +0800

    arm64

--- getg0_arm64.s
+++ getg0_arm64.s
@@ -3,17 +3,17 @@
 
 TEXT  ·getg0(SB), NOSPLIT, $32-16
     NO_LOCAL_POINTERS
-    MOVW $0, ret_type+0(FP)
-    MOVW $0, ret_data+8(FP)
+    MOVD $0, ret_type+0(FP)
+    MOVD $0, ret_data+8(FP)
     GO_RESULTS_INITIALIZED
 
     // get runtime.g type
-    MOVW $type·runtime·g(SB), R8
+    MOVD $type·runtime·g(SB), R8
 
     // get runtime·g0 variable
-    MOVW runtime·g0(SB), R9
+    MOVD runtime·g0(SB), R9
 
     // return interface{}
-    MOVW R8, ret_type+0(FP)
-    MOVW R9, ret_data+8(FP)
+    MOVD R8, ret_type+0(FP)
+    MOVD R9, ret_data+8(FP)
     RET
