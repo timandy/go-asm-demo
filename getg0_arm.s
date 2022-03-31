146ea858caeb28a3ad1f02ce2b00a6a38d1cbcb4
Tim <xuchonglei@126.com>
2022-03-31 23:49:50 +0800

    Update getg0_arm.s

--- getg0_arm.s
+++ getg0_arm.s
@@ -3,8 +3,8 @@
 
 TEXT  ·getg0(SB), NOSPLIT, $8-8
     NO_LOCAL_POINTERS
-    MOVW $0, ret_type+0(FP)
-    MOVW $0, ret_data+8(FP)
+    MOVW $0, ret_lo+0(FP)
+    MOVW $0, ret_hi+4(FP)
 
     // get runtime.g type
     MOVW $type·runtime·g(SB), R0
@@ -13,6 +13,6 @@ TEXT  ·getg0(SB), NOSPLIT, $8-8
     MOVW $0, R1
 
     // return interface{}
-    MOVW R0, ret_type+0(FP)
-    MOVW R1, ret_data+8(FP)
+    MOVW R0, ret_lo+0(FP)
+    MOVW R1, ret_hi+4(FP)
     RET
