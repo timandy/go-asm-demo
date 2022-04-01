ccb5e5d25a829e91bc31a6df06ead8a3c3f853e6
TimAndy <xuchonglei@126.com>
2022-04-01 16:48:14 +0800

    add test

--- gohack_test.go
+++ gohack_test.go
@@ -3,7 +3,7 @@ package gohack
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
-	_ "unsafe"
+	"unsafe"
 )
 
 func TestGoid(t *testing.T) {
@@ -12,6 +12,69 @@ func TestGoid(t *testing.T) {
 	})
 }
 
+func TestPaniconfault(t *testing.T) {
+	runTest(t, func() {
+		gp := getg()
+		//read-1
+		assert.False(t, setPanicOnFault(false))
+		assert.False(t, gp.getPanicOnFault())
+		//read-2
+		setPanicOnFault(true)
+		assert.True(t, gp.getPanicOnFault())
+		//write-1
+		gp.setPanicOnFault(false)
+		assert.False(t, setPanicOnFault(false))
+		//write-2
+		gp.setPanicOnFault(true)
+		assert.True(t, setPanicOnFault(true))
+		//write-read-1
+		gp.setPanicOnFault(false)
+		assert.False(t, gp.getPanicOnFault())
+		//write-read-2
+		gp.setPanicOnFault(true)
+		assert.True(t, gp.getPanicOnFault())
+	})
+}
+
+func TestProfLabel(t *testing.T) {
+	runTest(t, func() {
+		ptr := unsafe.Pointer(&struct{}{})
+		null := unsafe.Pointer(uintptr(0))
+		assert.NotEqual(t, ptr, null)
+		//
+		gp := getg()
+		//read-1
+		assert.Equal(t, null, getProfLabel())
+		assert.Equal(t, null, gp.getLabel())
+		//read-2
+		setProfLabel(ptr)
+		assert.Equal(t, ptr, gp.getLabel())
+		//write-1
+		gp.setLabel(nil)
+		assert.Equal(t, null, getProfLabel())
+		//write-2
+		gp.setLabel(ptr)
+		assert.Equal(t, ptr, getProfLabel())
+		//write-read-1
+		gp.setLabel(nil)
+		assert.Equal(t, null, gp.getLabel())
+		//write-read-2
+		gp.setLabel(ptr)
+		assert.Equal(t, ptr, gp.getLabel())
+	})
+}
+
 // curGoroutineID parse the current g's goid from caller stack.
 //go:linkname curGoroutineID net/http.http2curGoroutineID
 func curGoroutineID() int64
+
+//go:linkname setPanicOnFault runtime/debug.setPanicOnFault
+func setPanicOnFault(new bool) (old bool)
+
+// getProfLabel get current g's labels which will be inherited by new goroutine.
+//go:linkname getProfLabel runtime/pprof.runtime_getProfLabel
+func getProfLabel() unsafe.Pointer
+
+// setProfLabel set current g's labels which will be inherited by new goroutine.
+//go:linkname setProfLabel runtime/pprof.runtime_setProfLabel
+func setProfLabel(labels unsafe.Pointer)
